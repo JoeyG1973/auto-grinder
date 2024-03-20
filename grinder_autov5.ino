d25e76800ee1dc1fecd9f4bed560bbe7fac357b9
Joseph Garcia <joejgarcia@gmail.com>
2024-03-20 15:32:26 -0400

    parameter adjustments

--- grinder_autov5.ino
+++ grinder_autov5.ino
@@ -4,27 +4,28 @@
 #include <SmoothProgress.h>
 #define BAR_STYLES_IN_PROGMEM
 #include <BarStyleV2.h>
-
+#include <avr/wdt.h>
 
 #define X_LIMIT_SWITCH_LEFT_PIN 51
 #define X_LIMIT_SWITCH_RIGHT_PIN 53
 #define X_STEP_PIN 6
 #define X_DIR_PIN 5
 #define X_ENA_PIN 4
-#define X_MAX_SPEED 30000 // in hertz/steps
+#define X_MAX_SPEED 10000 // in hertz/steps
 #define X_POTPIN A0
 #define X_ENABLE_SWITCH_PIN 3
-
+#define X_ACCEL 150000
 
 #define Y_STEP_PIN 8
 #define Y_DIR_PIN 9
 #define Y_ENA_PIN 10
-#define Y_MAX_SPEED 37558 // in hertz/steps
+#define Y_MAX_SPEED 25000 // in hertz/steps
 #define Y_POTPIN A4
-#define Y_MAX_STEP_OVER 100
+#define Y_MAX_STEP_OVER 10000
 #define Y_ENABLE_SWITCH_PIN 2
 #define Y_LIMIT_SWITCH_FRONT_PIN 50
 #define Y_LIMIT_SWITCH_REAR_PIN 52
+#define Y_ACCEL 200000
 
 ///////////////////////////////////////
 // buttons, switches, sensors objects //
@@ -57,6 +58,7 @@ char buffer[16];
 bool startup = true;
 bool Y_limit_switch_front_trip = false, Y_limit_switch_rear_trip = false;
 bool X_limit_switch_left_trip = false, X_limit_switch_right_trip = false;
+
 ////////////////////////
 // lcd global objects //
 ////////////////////////
@@ -70,27 +72,27 @@ void setup()
   ///////////////////////////////////////////
   // button, switch, sensor, pot, etc init //
   ///////////////////////////////////////////
-  X_limit_switch_left.attach( X_LIMIT_SWITCH_LEFT_PIN, INPUT );
-  X_limit_switch_left.interval(5);
+  X_limit_switch_left.attach(X_LIMIT_SWITCH_LEFT_PIN, INPUT);
+  X_limit_switch_left.interval(1);
   X_limit_switch_left.setPressedState(HIGH);
 
-  X_limit_switch_right.attach( X_LIMIT_SWITCH_RIGHT_PIN, INPUT );
-  X_limit_switch_right.interval(5);
+  X_limit_switch_right.attach(X_LIMIT_SWITCH_RIGHT_PIN, INPUT);
+  X_limit_switch_right.interval(1);
   X_limit_switch_right.setPressedState(HIGH);
 
-  X_toggle_switch.attach( X_ENABLE_SWITCH_PIN, INPUT_PULLUP);
+  X_toggle_switch.attach(X_ENABLE_SWITCH_PIN, INPUT_PULLUP);
   X_toggle_switch.interval(5);
   X_toggle_switch.setPressedState(LOW);
 
-  Y_toggle_switch.attach( Y_ENABLE_SWITCH_PIN, INPUT_PULLUP );
+  Y_toggle_switch.attach(Y_ENABLE_SWITCH_PIN, INPUT_PULLUP);
   Y_toggle_switch.interval(5);
   Y_toggle_switch.setPressedState(LOW);
 
-  Y_limit_switch_front.attach( Y_LIMIT_SWITCH_FRONT_PIN, INPUT );
+  Y_limit_switch_front.attach(Y_LIMIT_SWITCH_FRONT_PIN, INPUT);
   Y_limit_switch_front.interval(5);
   Y_limit_switch_front.setPressedState(HIGH);
 
-  Y_limit_switch_rear.attach( Y_LIMIT_SWITCH_REAR_PIN, INPUT );
+  Y_limit_switch_rear.attach(Y_LIMIT_SWITCH_REAR_PIN, INPUT);
   Y_limit_switch_rear.interval(5);
   Y_limit_switch_rear.setPressedState(HIGH);
 
@@ -106,25 +108,24 @@ void setup()
   Xstepper->setEnablePin(X_ENA_PIN, false);
   Xstepper->setAutoEnable(false);
   Xstepper->setSpeedInHz(0);
-//  Xstepper->setAcceleration(100000);
-  Xstepper->setAcceleration(200000);
+  Xstepper->setAcceleration(X_ACCEL);
 
   Ystepper = engine.stepperConnectToPin(Y_STEP_PIN);
   Ystepper->setDirectionPin(Y_DIR_PIN);
   Ystepper->setEnablePin(Y_ENA_PIN, false);
   Ystepper->setAutoEnable(false);
   Ystepper->setSpeedInHz(Y_MAX_SPEED);
-  Ystepper->setAcceleration(3500);
+  Ystepper->setAcceleration(Y_ACCEL);
 
   //////////////
   // lcd init //
   //////////////
   lcd.init();
-  delay(2000);
   lcd.clear();
   dispA.begin();
 
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 3; i++)
+  {
     lcd.backlight();
     delay(250);
     lcd.noBacklight();
@@ -134,8 +135,12 @@ void setup()
   lcd.home();
   lcd.setCursor(1, 0);
   lcd.print("X Axis:");
+  lcd.setCursor(3, 1);
+  lcd.print("OFF");
   lcd.setCursor(11, 0);
   lcd.print("Y Axis:");
+  lcd.setCursor(13, 1);
+  lcd.print("OFF");
   lcd.setCursor(0, 2);
   lcd.print("D: ");
   lcd.setCursor(3, 2);
@@ -145,13 +150,11 @@ void setup()
   lcd.setCursor(13, 2);
   lcd.print("Fwd");
 
-//  Serial.begin(115200);
-//  Serial.println(Xstepper->getMaxSpeedInUs());
-//  Serial.println(Xstepper->getMaxSpeedInTicks());
-//  Serial.println(Xstepper->getMaxSpeedInHz());
-//  Serial.println(Xstepper->getMaxSpeedInMilliHz());
+  //////////////
+  // Watchdog //
+  //////////////
+  wdt_enable(WDTO_250MS);
 }
-
 
 void loop()
 {
@@ -182,7 +185,7 @@ void loop()
   ///////////
   X_pot_EMA_S = potEMA(X_POTPIN, X_pot_EMA_S);
   X_pot_map = map(X_pot_EMA_S, 0, 1019, 0, X_MAX_SPEED);
-//  Serial.println(X_pot_map);
+  //  Serial.println(X_pot_map);
   barX = map(X_pot_EMA_S, 0, 1019, 0, 100);
 
   ///////////
@@ -191,9 +194,12 @@ void loop()
   Y_pot_EMA_S = potEMA(Y_POTPIN, Y_pot_EMA_S);
   Y_pot_map = map(Y_pot_EMA_S, 0, 1019, 0, Y_MAX_STEP_OVER);
   barY = map(Y_pot_EMA_S, 0, 1019, 0, 100);
-  if (Y_step_with_dir < 0) {
+  if (Y_step_with_dir < 0)
+  {
     Y_step_with_dir = int(Y_pot_map) * -1;
-  } else {
+  }
+  else
+  {
     Y_step_with_dir = int(Y_pot_map);
   }
 
@@ -201,11 +207,14 @@ void loop()
   // Y toggle //
   //////////////
 
-  if ( Y_toggle_switch.isPressed() ) {
+  if (Y_toggle_switch.isPressed())
+  {
     Ystepper->enableOutputs();
     lcd.setCursor(13, 1);
     lcd.print("ON ");
-  } else {
+  }
+  else
+  {
     Ystepper->disableOutputs();
     lcd.setCursor(13, 1);
     lcd.print("OFF");
@@ -215,27 +224,32 @@ void loop()
   // X toggle //
   //////////////
   // don't enable X unless the pot is at 0
-  if ( X_toggle_switch.isPressed() && X_pot_EMA_S == 0 ) {
+  if (X_toggle_switch.isPressed() && X_pot_EMA_S == 0)
+  {
     Xstepper->enableOutputs();
     lcd.setCursor(3, 1);
-    lcd.print("ON  ");
-  } else if (X_toggle_switch.isPressed() == false) {
+    lcd.print("ON ");
+  }
+  else if (X_toggle_switch.isPressed() == false)
+  {
     Xstepper->disableOutputs();
     lcd.setCursor(3, 1);
-    lcd.print("OFF ");
+    lcd.print("OFF");
   }
 
   //////////////
   // Y Limit  //
   //////////////
-  if (Y_limit_switch_front.pressed() && Y_limit_switch_front_trip == false) {
+  if (Y_limit_switch_front.pressed() && Y_limit_switch_front_trip == false)
+  {
     Y_step_with_dir = Y_step_with_dir * -1;
     Y_limit_switch_front_trip = true;
     Y_limit_switch_rear_trip = false;
     lcd.setCursor(13, 2);
     lcd.print("Rev");
   }
-  if (Y_limit_switch_rear.pressed() && Y_limit_switch_rear_trip == false) {
+  if (Y_limit_switch_rear.pressed() && Y_limit_switch_rear_trip == false)
+  {
 
     Y_step_with_dir = Y_step_with_dir * -1;
     Y_limit_switch_rear_trip = true;
@@ -247,9 +261,11 @@ void loop()
   /////////////
   // X limit //
   /////////////
-  
-  if (X_limit_switch_left.pressed() && X_limit_switch_right_trip == false) {
-    if (X_toggle_switch.isPressed() && X_pot_EMA_S > 0) {
+
+  if (X_limit_switch_left.pressed() && X_limit_switch_right_trip == false)
+  {
+    if (X_toggle_switch.isPressed() && X_pot_EMA_S > 0)
+    {
       Xstepper->runBackward();
     }
     X_dir_forward = true;
@@ -257,12 +273,15 @@ void loop()
     X_limit_switch_right_trip = true;
     lcd.setCursor(3, 2);
     lcd.print("Right");
-    if (Y_toggle_switch.isPressed() && Y_pot_EMA_S > 0) {
+    if (Y_toggle_switch.isPressed() && Y_pot_EMA_S > 0)
+    {
       Ystepper->move(Y_step_with_dir);
     }
   }
-  if (X_limit_switch_right.pressed() && X_limit_switch_left_trip == false)  {
-    if (X_toggle_switch.isPressed() && X_pot_EMA_S > 0) {
+  if (X_limit_switch_right.pressed() && X_limit_switch_left_trip == false)
+  {
+    if (X_toggle_switch.isPressed() && X_pot_EMA_S > 0)
+    {
       Xstepper->runForward();
     }
     X_dir_forward = false;
@@ -270,11 +289,11 @@ void loop()
     X_limit_switch_left_trip = true;
     lcd.setCursor(3, 2);
     lcd.print("Left ");
-    if (Y_toggle_switch.isPressed() && Y_pot_EMA_S > 0) {
+    if (Y_toggle_switch.isPressed() && Y_pot_EMA_S > 0)
+    {
       Ystepper->move(Y_step_with_dir);
     }
   }
-
 
   /////////////////////
   // X Movement Stop //
@@ -285,10 +304,13 @@ void loop()
   // way to do this by combining it with the
   // X Movement Start and not have this code run each
   // cycle and constantly send Xstepper->stopMove()
-  if (X_pot_EMA_S == 0 && startup == false) {
+  if (X_pot_EMA_S == 0 && startup == false)
+  {
     Xstepper->stopMove();
     startup = true;
-  } else if ( X_pot_EMA_S > 0 && startup == true) {
+  }
+  else if (X_pot_EMA_S > 0 && startup == true)
+  {
     startup = false;
   }
 
@@ -296,27 +318,27 @@ void loop()
   // X Movement Start //
   //////////////////////
 
-  if (X_toggle_switch.isPressed() && X_pot_EMA_S > 0) {
+  if (X_toggle_switch.isPressed() && X_pot_EMA_S > 0)
+  {
     Xstepper->setSpeedInHz(X_pot_map);
-    switch (X_dir_forward) {
-      case false:
-        Xstepper->runForward();
-        break;
-      case true:
-        Xstepper->runBackward();
-        break;
+    switch (X_dir_forward)
+    {
+    case false:
+      Xstepper->runForward();
+      break;
+    case true:
+      Xstepper->runBackward();
+      break;
     }
     startup = false;
-
   }
 
-
+  wdt_reset();
   delay(1);
-
 }
 
-
-uint32_t potEMA(int sensorPin, uint32_t EMA_S) {
+uint32_t potEMA(int sensorPin, uint32_t EMA_S)
+{
   float EMA_a = 0.3;
   int sensorValue = analogRead(sensorPin);
   EMA_S = (EMA_a * sensorValue) + ((1 - EMA_a) * EMA_S);
